3af027a8e97cdb2287e323a38245957cf7a8fedb
dasoslabs <dasoslab@gmail.com>
2024-06-21 11:33:25 +0900

    💄 경쟁력 본사지원 텍스트에 마진 추가

--- src/app/brand/tab/support.jsx
+++ src/app/brand/tab/support.jsx
@@ -6,7 +6,7 @@ export default function SupportTab() {
   return (
     <ul className="py-40 max-w-pc m-auto grid grid-cols-4 gap-6">
       {supportCards.map(({ text, imgUrl, imgAlt }, idx) => (
-        <li key={text} className="mb-3">
+        <li key={text} className="mb-2">
           <Image
             width="342"
             height="240"
@@ -14,7 +14,7 @@ export default function SupportTab() {
             alt={imgAlt}
             quality={100}
           />
-          <h3 className="text-xl">
+          <h3 className="text-xl mt-6">
             <span className="font-bold mr-2">
               {(idx < 10 ? "0" : "") + (idx + 1)}.
             </span>
