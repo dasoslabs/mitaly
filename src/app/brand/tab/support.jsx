793c9bc95a0660409a71ca28e95ff3f11fd32b2e
dasoslabs <dasoslab@gmail.com>
2024-06-21 14:34:58 +0900

    📱 경쟁력 본사지원 탭 모바일

--- src/app/brand/tab/support.jsx
+++ src/app/brand/tab/support.jsx
@@ -4,9 +4,9 @@ import { supportCards } from "../data"
 
 export default function SupportTab() {
   return (
-    <ul className="py-40 max-w-pc m-auto grid grid-cols-4 gap-6">
+    <ul className="py-10 px-6 lg:py-40 max-w-pc m-auto grid grid-cols-2 gap-5 lg:grid-cols-4 lg:gap-6">
       {supportCards.map(({ text, imgUrl, imgAlt }, idx) => (
-        <li key={text} className="mb-2">
+        <li key={text} className="lg:mb-2">
           <Image
             width="342"
             height="240"
@@ -14,7 +14,7 @@ export default function SupportTab() {
             alt={imgAlt}
             quality={100}
           />
-          <h3 className="text-xl mt-6">
+          <h3 className="text-sm lg:text-xl mt-3 lg:mt-6">
             <span className="font-bold mr-2">
               {(idx < 10 ? "0" : "") + (idx + 1)}.
             </span>
