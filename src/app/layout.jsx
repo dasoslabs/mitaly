b89462cd0809b07d9dabcd867192be2b067ad3df
dasoslabs <dasoslab@gmail.com>
2024-06-30 08:00:24 +0900

    🍱 favicon 추가

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,8 +12,37 @@ const pretendard = localFont({
 })
 
 export const metadata = {
-  title: "미태리",
-  description: "미태리 홈페이지",
+  title: "미태리(Mitaly) - 파스타, 스파게티 전문점",
+  description:
+    "365일 줄서는 파스타 맛집! 5년 연속 소비자만족지수 1위! 가장 빠른 성공 창업 미태리에서 시작하세요! 업종변경/소자본/초보창업 강력 추천",
+  author: "미태리",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicon/apple-touch-icon.png",
+    },
+  ],
+  openGraph: {
+    title: "미태리(Mitaly) - 파스타, 스파게티 전문점",
+    site_name: "미태리(Mitaly) - 파스타, 스파게티 전문점",
+    url: "https://mitaly.co.kr",
+    description:
+      "365일 줄서는 파스타 맛집! 5년 연속 소비자만족지수 1위! 가장 빠른 성공 창업 미태리에서 시작하세요! 업종변경/소자본/초보창업 강력 추천",
+    type: "website",
+  },
 }
 
 export default function RootLayout({ children }) {
