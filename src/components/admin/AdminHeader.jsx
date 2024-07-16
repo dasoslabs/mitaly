bb4273477fe1f71cb54a3ea8951f84a5d6b8e645
dasoslabs <dasoslab@gmail.com>
2024-07-16 15:29:59 +0900

    🚧 로그인 사용자 이름 표시

--- src/components/admin/AdminHeader.jsx
+++ src/components/admin/AdminHeader.jsx
@@ -1,10 +1,18 @@
-export default function AdminHeader() {
+"use server"
+
+import createSupabase from "@/libs/supabase"
+
+export default async function AdminHeader() {
+  const supabase = createSupabase()
+  const { data: { user } } = await supabase.auth.getUser()
+  // const { data } = await supabase.from("users").select("id, name, nickname").eq("user_id", user.id).single()
+
   return (
     <header className="fixed top-0 left-0 w-full h-20 border-b border-light-gray bg-white">
       <div className="h-full flex justify-between items-center max-w-screen-lg m-auto">
         <h1 className="text-2xl font-bold">관리자 센터</h1>
         <div className="flex justify-center items-center space-x-8">
-          <p>{"name"}님</p>
+          <p>{user.user_metadata?.full_name}님</p>
           <form action="/api/auth/logout" method="post">
             <button className="bg-black text-white py-2 px-5">
               로그아웃
