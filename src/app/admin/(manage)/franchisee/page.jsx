31f7ab3dd218789684d0210f8230897c384bca4b
dasoslabs <dasoslab@gmail.com>
2024-07-18 11:51:41 +0900

    🚧 가맹문의 페이지

--- src/app/admin/(manage)/franchisee/page.jsx
+++ src/app/admin/(manage)/franchisee/page.jsx
@@ -1,3 +1,20 @@
-export default function AdminFranchiseePage() {
-  return <section>가맹문의 관리 페이지</section>
+"use server"
+
+// import { getAllStores } from "@/libs/db/store"
+import StoreList from "@/components/admin/store/StoreList"
+
+export default async function AdminFranchiseePage() {
+  // const stores = await getAllStores()
+
+  return (
+    <>
+      <section>
+        <h2 className="font-semibold text-2xl">가맹문의</h2>
+      </section>
+
+      <section>
+        {/* <StoreList list={stores} /> */}
+      </section>
+    </>
+  )
 }
