d6329548fbeb2184c795024aaa73a358b6dcdc30
dasoslabs <dasoslab@gmail.com>
2024-07-19 22:22:37 +0900

    📱 메인 배경 영상 모바일 대응

--- src/components/common/VideoPlayer.jsx
+++ src/components/common/VideoPlayer.jsx
@@ -12,16 +12,18 @@ export default function VideoPlayer({ muted = true }) {
 
   return (
     isWindow && (
-      <div className="w-full h-screen">
-        <ReactPlayer
-          url="https://www.youtube-nocookie.com/embed/S8xmkLL6KVc?si=G6dYXM2bwotV_cBv"
-          playing
-          loop
-          controls={false}
-          muted={muted}
-          width="1800px"
-          height="100vh"
-        />
+      <div className="w-full h-screen overflow-hidden">
+        <div className="-ml-32 lg:ml-0 w-[1800px] h-full">
+          <ReactPlayer
+            url="https://www.youtube-nocookie.com/embed/S8xmkLL6KVc?si=G6dYXM2bwotV_cBv"
+            playing
+            loop
+            controls={false}
+            muted={muted}
+            width="100%"
+            height="100vh"
+          />
+        </div>
       </div>
     )
   )
