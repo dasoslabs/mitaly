988fd06b2745639d095c6b318adb75db28439f63
dasoslabs <dasoslab@gmail.com>
2024-07-20 20:37:20 +0900

    💄 비디오 위치 수정

--- src/components/common/VideoPlayer.jsx
+++ src/components/common/VideoPlayer.jsx
@@ -13,11 +13,11 @@ export default function VideoPlayer({ muted = true }) {
   return (
     isWindow && (
       <div className="w-full h-screen overflow-hidden">
-        <div className="-ml-32 lg:ml-0 w-[1800px] h-full">
+        <div className="-ml-80 lg:ml-0 w-[1800px] h-full">
           <ReactPlayer
             url="https://www.youtube-nocookie.com/embed/S8xmkLL6KVc?si=G6dYXM2bwotV_cBv"
             playing
-            loop
+            loop={true}
             controls={false}
             muted={muted}
             width="100%"
