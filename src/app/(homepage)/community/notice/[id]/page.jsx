35d187644507db447da2a7624a901bc5bc9a1bec
dasoslabs <dasoslab@gmail.com>
2024-07-20 10:36:57 +0900

    🔊 공지사항 id 조회 테스트

--- src/app/(homepage)/community/notice/[id]/page.jsx
+++ src/app/(homepage)/community/notice/[id]/page.jsx
@@ -9,25 +9,28 @@ export default async function NoticeDetailPage() {
   const headersList = headers()
   const headerPathname = headersList.get("x-pathname") || ""
   const id = headerPathname.split("/community/notice/")[1]
-  const post = await getPostDetailById(id)
+  // const post = await getPostDetailById(id)
+  console.log(id)
 
-  return (
-    <section className="space-y-12 max-w-screen-lg m-auto my-10 lg:my-36">
-      <div className="space-y-4 pb-6 border-b border-[#D9D9D9]">
-        <h2 className="font-extralight text-2xl lg:text-[40px]">
-          {post.title}
-        </h2>
-        <p className="text-[#666666]">{post.created_at}</p>
-      </div>
-      <div>
-        <HtmlParser>{post.content}</HtmlParser>
-      </div>
-      <Link
-        href="./"
-        className="inline-block bg-white border border-black py-1 px-4 rounded-full border border-light-gray"
-      >
-        목록으로 돌아가기
-      </Link>
-    </section>
-  )
+  return null
+
+  // return (
+  //   <section className="space-y-12 max-w-screen-lg m-auto my-10 lg:my-36">
+  //     <div className="space-y-4 pb-6 border-b border-[#D9D9D9]">
+  //       <h2 className="font-extralight text-2xl lg:text-[40px]">
+  //         {post.title}
+  //       </h2>
+  //       <p className="text-[#666666]">{post.created_at}</p>
+  //     </div>
+  //     <div>
+  //       <HtmlParser>{post.content}</HtmlParser>
+  //     </div>
+  //     <Link
+  //       href="./"
+  //       className="inline-block bg-white border border-black py-1 px-4 rounded-full border border-light-gray"
+  //     >
+  //       목록으로 돌아가기
+  //     </Link>
+  //   </section>
+  // )
 }
