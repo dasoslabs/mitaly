8407d309e09ff1223ea3c1d479ed4cbd195c993c
dasoslabs <dasoslab@gmail.com>
2024-07-30 11:29:42 +0900

    🔧 supabase 도메인 환경변수로 주입

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["stejwznngdteutifdomy.supabase.co"],
+    domains: [`${process.env.SUPABASE_DOMAIN}`],
   },
 }
 
